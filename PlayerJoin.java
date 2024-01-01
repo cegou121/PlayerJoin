83b8ffd1405f3eb028162ee576655f9544a9ec5e
De1fy <103572164+cegou121@users.noreply.github.com>
2024-01-01 14:01:47 +0200

    Update PlayerJoin.java

--- PlayerJoin.java
+++ PlayerJoin.java
@@ -1,10 +1,6 @@
 package org.example.ru.playerjoin;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.boss.BarColor;
-import org.bukkit.boss.BarStyle;
-import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,46 +9,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerJoin extends JavaPlugin implements Listener {
 
-    private BossBar bossBar;
-
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
-
-        // Создаем босс-бар
-        bossBar = Bukkit.createBossBar(
-                ChatColor.YELLOW + "Разработка от De1fy",
-                BarColor.YELLOW,
-                BarStyle.SOLID
-        );
-        bossBar.setVisible(true);
-
-        // обновление босс-бара каждую секунду
-        Bukkit.getScheduler().runTaskTimer(this, this::updateBossBar, 0, 20);
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("PlayerJoin включился!");
+    }
+    
+    @Override
+    public void onDisable() {
+        getLogger().info("PlayerJoin отключился");
     }
 
-    private void updateBossBar() {
-        // Обновляем босс-бар для всех онлайн игроков
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            bossBar.addPlayer(player);
-        }
-    }
-    @Ovveride
-    public void OnlineJoin
-    if (Player player) = bukkit.getplayer() {
-        String . getjoin() {
-            Player player = Player ()
-                bossbar.addPlayer(bukkit.GetLoger);
-
-    @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        String playerName = player.getName();
-
-        // Отправка красивого сообщения при присоединении игрока
-        player.sendMessage(ChatColor.GREEN + "Добро пожаловать, " + playerName + "!");
-
-        // Добавляем игрока в босс-бар при присоединении
-        bossBar.addPlayer(player);
+        player.sendMessage("Добро пожаловать на сервер!");
     }
 }
